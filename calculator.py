a8d6382f7fe29964a4771103b5f6c647d553b053
mbarnestech <maggie.barnes@gmail.com>
2023-03-01 10:48:01 -0700

    Initial commit

--- calculator.py
+++ calculator.py
@@ -3,5 +3,18 @@
 from arithmetic import (add, subtract, multiply, divide, square, cube,
                         power, mod, )
 
+# Run program until 'break' is called
+while True:
 
-# Replace this with your code
+    # Get input
+    user_input = input("Enter your equation: ")
+    print(user_input)
+
+    # Check if input contains 'q' or 'quit'
+    if 'q' in user_input.lower() or 'quit' in user_input.lower():
+        print('Thank you for using the calculator. Goodbye.')
+        break
+
+    # Tokenize input
+
+    # Execute appropriate function
